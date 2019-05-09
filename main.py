cfec505f3e45ae5e7ccece0dfd61ce25aa8cb0ab
Eugenio Contreras Sánchez <ecs.keni@gmail.com>
2019-05-09 11:24:26 -0400

    Add function request_event to main.py
    Function that return a json with a list of events availables at the moment in meetup api.
    Filtered by topic and country. Those parameters at the moment are require.

--- main.py
+++ main.py
@@ -1,18 +1,51 @@
 import os
 import pandas as pd
+import requests
+import configparser
+import numpy as np
+import pandas as pd
+from opencage.geocoder import OpenCageGeocoder
 from event import Event
 
+# ConfigParser
+config = configparser.ConfigParser()
+config.read('config/meetup.cfg')
 
-os.environ['MEETUP_API_KEY'] = '1e742615172342564c18644174823'
+# Environ variables for API KEY
+os.environ['MEETUP_API_KEY'] = config['MEETUP']['API_KEY']
+os.environ['OPENCAGE_KEY'] = config['OPENCAGE']['KEY']
 
-default_args = dict(country='United Stated',
-                    key=os.environ['MEETUP_API_KEY'], zip='10012', format='json')
-url_path = "https://api.meetup.com/2/open_events"
+# Initialize OpenCage Geocode
+geocode = OpenCageGeocoder(os.environ['OPENCAGE_KEY'])
 
-event = Event(params=default_args, url_request=url_path)
+# Function for request events
 
-json_response = event.get_events()
+# function for request meetup API
 
-df = pd.read_json(json_response)
 
-print(json_response)
+def request_event(topic, country=None, url_path):
+    '''
+    Function that make a request to meetup api and return a json from all events filtered by topic and country
+
+    Args:
+        topic (string): Topic of the events
+        country (string): Country of the events
+        url_path (string): Url for meetup API https://www.meetup.com/es-ES/meetup_api/docs/
+    '''
+    default_args = dict(
+        country=country,
+        topic=topic,
+        key=os.environ['MEETUP_API_KEY']
+    )
+
+    r = requests.get(url_path, params=default_args)
+    results = r.text
+    return json.loads(results)
+
+
+# url meetup request
+url_meetup_api = "https://api.meetup.com/2/open_events"
+# get list of events by topic and country
+list_events = request_event(topic="Python", country=None, url_meetup_api)
+# get data from json response
+data = list_events['results']
