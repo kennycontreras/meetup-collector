5822058bf57a16454a9dd6c413201cb1a1b61bfd
Eugenio Contreras Sánchez <ecs.keni@gmail.com>
2019-05-10 06:36:32 -0400

    Add functions for better structure of code
    spark function: create spark session
    dataframe function: return a Spark Dataframe based on meetup json response

--- main.py
+++ main.py
@@ -15,11 +15,6 @@ from pyspark.sql.functions import udf
 config = configparser.ConfigParser()
 config.read('config/meetup.cfg')
 
-# SparkSession
-spark = SparkSession \
-    .builder \
-    .config("spark.jars.packages", "org.apache.hadoop:hadoop-aws:2.7.5") \
-    .getOrCreate()
 
 # Environ variables for API KEY
 os.environ['MEETUP_API_KEY'] = config['MEETUP']['API_KEY']
@@ -27,6 +22,15 @@ os.environ['OPENCAGE_KEY'] = config['OPENCAGE']['KEY']
 
 # Initialize OpenCage Geocode
 geocode = OpenCageGeocoder(os.environ['OPENCAGE_KEY'])
+
+
+def spark_session():
+    spark = SparkSession \
+        .builder \
+        .config("spark.jars.packages", "org.apache.hadoop:hadoop-aws:2.7.5") \
+        .getOrCreate()
+
+    return spark
 
 
 def request_event(topic, country=None, url_path):
@@ -47,74 +51,81 @@ def request_event(topic, country=None, url_path):
 
     r = requests.get(url_path, params=default_args)
     results = r.text
-    return json.loads(results)
+    data = json.loads(results)
+    return data['results']
 
 
-# url meetup request
-url_meetup_api = "https://api.meetup.com/2/open_events"
-# get list of events by topic and country
-list_events = request_event(topic="Python", country=None, url_meetup_api)
-# get data from json response
-data = list_events['results']
+def build_dataframe(spark, data):
+    '''
+    Build a SparkDataframe based on json response.
+    Args:
+        Spark (object): Spark Session
+        data (json array): Event data from meetup api
 
-### BUILD DATA FRAME ###
-# columns
-columns = ['id', 'date', 'year', 'month', 'day', 'country', 'city', 'state,', 'address',
-           'meetup_name', 'meetup_group_name', 'description', 'event_url', 'yes_rsvp_count', 'status']
-id, date, year, month, day, country, city, state, address, meetup_name, meetup_group_name, description, event_url, yes_rsvp_count, status = ([
-] for i in range(15))
+    '''
 
-for label in data:
-    date_event = datetime.fromtimestamp(label['time'] / 1000.0)
+    # Columns to build data frame
+    columns = ['id', 'date', 'year', 'month', 'day', 'country', 'city', 'state,', 'address',
+               'meetup_name', 'meetup_group_name', 'description', 'event_url', 'yes_rsvp_count', 'status']
+    id, date, year, month, day, country, city, state, address, meetup_name, meetup_group_name, description, event_url, yes_rsvp_count, status = ([
+    ] for i in range(15))
 
-    id.append(label['id'])
-    date.append(date_event)
-    year.append(date_event.year)
-    month.append(date_event.month)
-    day.append(date_event.year)
+    # Iterate over events
+    for label in data:
+        date_event = datetime.fromtimestamp(label['time'] / 1000.0)
 
-    if label.get('venue'):
-        country.append(label['venue'].get('country'))
-        city.append(label['venue'].get('city'))
-        state.append(label['venue'].get('state'))
-        address.append(label['venue'].get('address_1'))
-    else:
-        location_json = geocode.reverse_geocode(
-            label['group'].get('group_lat'), label['group'].get('group_lon'))
-        country.append(location_json[0]['components'].get('country_code'))
-        city.append(location_json[0]['components'].get('city'))
-        state.append(location_json[0]['components'].get('state'))
-        address.append(location_json[0].get('formatted'))
+        id.append(label['id'])
+        date.append(date_event)
+        year.append(date_event.year)
+        month.append(date_event.month)
+        day.append(date_event.year)
 
-    meetup_name.append(label.get('name'))
-    meetup_group_name.append(label['group'].get('name'))
-    description.append(label.get('description'))
-    event_url.append(label['event_url'])
-    yes_rsvp_count.append(label.get('yes_rsvp_count'))
-    status.append(label.get('status'))
+        if label.get('venue'):
+            country.append(label['venue'].get('country'))
+            city.append(label['venue'].get('city'))
+            state.append(label['venue'].get('state'))
+            address.append(label['venue'].get('address_1'))
+        else:
+            location_json = geocode.reverse_geocode(
+                label['group'].get('group_lat'), label['group'].get('group_lon'))
+            country.append(location_json[0]['components'].get('country_code'))
+            city.append(location_json[0]['components'].get('city'))
+            state.append(location_json[0]['components'].get('state'))
+            address.append(location_json[0].get('formatted'))
 
-# Schema Structure
-schema = T.StructType([
-    T.StructField("id", T.StringType(), True),
-    T.StructField("date", T.TimestampType(), True),
-    T.StructField("year", T.IntegerType(), True),
-    T.StructField("month", T.IntegerType(), True),
-    T.StructField("day", T.IntegerType(), True),
-    T.StructField("country", T.StringType(), True),
-    T.StructField("city", T.StringType(), True),
-    T.StructField("state", T.StringType(), True),
-    T.StructField("address", T.StringType(), True),
-    T.StructField("meetup_name", T.StringType(), True),
-    T.StructField("meetup_group_name", T.StringType(), True),
-    T.StructField("description", T.StringType(), True),
-    T.StructField("event_url", T.StringType(), True),
-    T.StructField("yes_rsvp_count", T.IntegerType(), True),
-    T.StructField("status", T.StringType(), True)
-])
+        meetup_name.append(label.get('name'))
+        meetup_group_name.append(label['group'].get('name'))
+        description.append(label.get('description'))
+        event_url.append(label['event_url'])
+        yes_rsvp_count.append(label.get('yes_rsvp_count'))
+        status.append(label.get('status'))
 
-df_pandas = pd.DataFrame(np.transpose([id, date, year, month, day, country, city, state, address,
-                                       meetup_name, meetup_group_name, description, event_url, yes_rsvp_count, status]), columns=columns)
-df = spark.createDataFrame(df_pandas, schema=schema)
+    # Schema Structure
+    schema = T.StructType([
+        T.StructField("id", T.StringType(), True),
+        T.StructField("date", T.TimestampType(), True),
+        T.StructField("year", T.IntegerType(), True),
+        T.StructField("month", T.IntegerType(), True),
+        T.StructField("day", T.IntegerType(), True),
+        T.StructField("country", T.StringType(), True),
+        T.StructField("city", T.StringType(), True),
+        T.StructField("state", T.StringType(), True),
+        T.StructField("address", T.StringType(), True),
+        T.StructField("meetup_name", T.StringType(), True),
+        T.StructField("meetup_group_name", T.StringType(), True),
+        T.StructField("description", T.StringType(), True),
+        T.StructField("event_url", T.StringType(), True),
+        T.StructField("yes_rsvp_count", T.IntegerType(), True),
+        T.StructField("status", T.StringType(), True)
+    ])
+
+    # Create pandas DataFrame using numpy.transpose() method
+    df_pandas = pd.DataFrame(np.transpose([id, date, year, month, day, country, city, state, address,
+                                           meetup_name, meetup_group_name, description, event_url, yes_rsvp_count, status]), columns=columns)
+    # Create Spark DataFrame
+    df = spark.createDataFrame(df_pandas, schema=schema)
+
+    return df
 
 
 ### DATA WRANGLING ###
@@ -133,3 +144,15 @@ def remove_tags_udf(text):
 # Apply udf functions
 df = df.withColumn('country', upper_udf(df.country))
 df = df.withColumn('description', remove_tags_udf(df.description))
+
+
+if __name__ == '__main__':
+
+    # Create Spark Session
+    spark = spark_session()
+    # url meetup request
+    url_meetup_api = "https://api.meetup.com/2/open_events"
+    # get list of events by topic and country
+    data = request_event(topic="Python", country=None, url_meetup_api)
+    # Build dataframe
+    df = build_dataframe(spark, data)
