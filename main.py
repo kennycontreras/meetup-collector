e11b7cf32b60c9b6368755bc02f27f889a7e4d71
Eugenio Contreras Sánchez <ecs.keni@gmail.com>
2019-05-13 11:05:41 -0400

    Modify request function
    Add for loop that make a request by zipcode. This zipcode list is provided by Zipcode class and a pandas dataframe

--- main.py
+++ main.py
@@ -37,7 +37,7 @@ def spark_session():
     return spark
 
 
-def request_event(topic, country, url_path):
+def request_event(topic, country, url_path, zipcode_list):
     '''
     Function that make a request to meetup api and return a json with all events availables
     filtered by topic and country
@@ -47,16 +47,27 @@ def request_event(topic, country, url_path):
         country (string): Country of the events
         url_path (string): Url for meetup API https://www.meetup.com/es-ES/meetup_api/docs/
     '''
-    default_args = dict(
-        country=country,
-        topic=topic,
-        key=os.environ['MEETUP_API_KEY']
-    )
 
-    r = requests.get(url_path, params=default_args)
-    results = r.text
-    data = json.loads(results)
-    return data['results']
+    data = []
+
+    for zipcode, city, state in zipcode_list:
+        default_args = dict(
+            zip=zipcode,
+            topic=topic,
+            key=os.environ['MEETUP_API_KEY']
+        )
+
+        print(default_args)
+
+        r = requests.get(url_path, params=default_args)
+        results = r.text
+        try:
+            json_data = json.loads(results)
+            data.append(json_data['results'])
+        except:
+            print('Error with file {}'.format(json_data))
+
+    return data
 
 
 if __name__ == '__main__':
@@ -66,15 +77,19 @@ if __name__ == '__main__':
     # url meetup request
     url_meetup_api = "https://api.meetup.com/2/open_events"
     csv_zipcode_path = ("../meetup-mongo/data/us-zip-code.csv")
-    # get list of events by topic and country
-    data = request_event(topic="Python", country="US", url_path=url_meetup_api)
+    # Initialize zipcode class
+    zipcode = Zipcode(csv_zipcode_path)
+    # create pandas dataframe
+    df_zipcode = zipcode.build_df()
+    # Zipcode list with zipcode, city and state for request
+    zipcode_list = zipcode.create_list(df_zipcode)
+    # get list of events by topic, country, zipcode, state and city
+    data = request_event(topic="Python", country="US",
+                         url_path=url_meetup_api, zipcode_list=zipcode_list)
+    print(data)
     # Build dataframe
-    event = Event(spark, data, geocode)
-    df_event = event.build_df()
-    print(df_event.count())
+    # event = Event(spark, data, geocode)
+    # df_event = event.build_df()
+    # print(df_event.count())
     # write data into mongodb cluster
     # write_data(df, spark)
-
-    zipcode = Zipcode(spark, csv_zipcode_path)
-    df_zipcode = zipcode.build_df()
-    print(df_zipcode.count())
