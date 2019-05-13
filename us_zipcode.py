66ed59aba44d1890de5681afc810fd65dead8c67
Eugenio Contreras Sánchez <ecs.keni@gmail.com>
2019-05-12 21:27:03 -0400

    Add zipcode class with dataframe function

--- us_zipcode.py
+++ us_zipcode.py
@@ -1,5 +1,12 @@
 
-def zipcode_dataframe(spark, csv_path):
 
-    df = spark.read.format('csv').option('header', 'true').load(csv_path)
-    return df.count()
+class Zipcode():
+
+    def __init__(self, spark, csv_path):
+        self.spark = spark
+        self.csv_path = csv_path
+
+    def zipcode_df(self, *args, **kwargs):
+
+        df = self.spark.read.format('csv').option('header', 'true').load(self.csv_path)
+        return df.count()
